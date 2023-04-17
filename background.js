495b48fc509bba18eea3956f88a6eb2b77d4e8cb
David Llanos <davidllanos22@gmail.com>
2023-04-17 16:07:35 +0200

    Stars with parallax

--- background.js
+++ background.js
@@ -1,5 +1,6 @@
 export class Background{
   constructor(){
+    this.mousePosition = {x: 0, y: 0};
     this.cvs = document.createElement("canvas");
     this.ctx = this.cvs.getContext("2d");
     this.cvs.style.width = "100%";
@@ -17,6 +18,15 @@ export class Background{
     this.init();
 
     resizeObserver.observe(this.cvs);
+
+    this.cvs.onmousemove = (event)=>{
+      this.mousePosition = {
+        x: event.clientX,
+        y: event.clientY
+      }
+    };
+
+    requestAnimationFrame(this._draw.bind(this));
   }
 
   init(){
@@ -28,15 +38,23 @@ export class Background{
   }
 
   draw(){
-
+    
   }
 
-  update(){
-    
+  _draw(){
+    this.draw();
+    requestAnimationFrame(this._draw.bind(this));
   }
   
   fillRect(x, y, w, h, color){
     this.ctx.fillStyle = color;
     this.ctx.fillRect(x, y, w, h);
+  }
+
+  fillCircle(x, y, r, color){
+    this.ctx.fillStyle = color;
+    this.ctx.beginPath();
+    this.ctx.arc(x, y, r, 0, 2 * Math.PI);
+    this.ctx.fill();
   }
 }  
--- backgrounds/stars.js
+++ backgrounds/stars.js
@@ -1,15 +1,36 @@
 import { Background } from "../background.js";
 
 class StarsBackground extends Background{
-  resize(){
-    this.fillRect(0, 0, this.cvs.width, this.cvs.height, "black");
-    
-    for(let i = 0; i < 100; i++){
-      let x = Math.random() * this.cvs.width;
-      let y = Math.random() * this.cvs.height;
-  
-      this.fillRect(x, y, 1, 1, "white");
+  init(){
+    this.stars = [];
+    let centerX = this.cvs.width / 2;
+    let centerY = this.cvs.height / 2;
+
+    for(let i = 0; i < 1000; i++){
+      let x = Math.random() * this.cvs.width * 2 - centerX;
+      let y = Math.random() * this.cvs.height * 2 - centerY;
+      let r = Math.random() * 1 + 0.1;
+      this.stars.push({x, y, r});
     }
+  }
+
+  resize(){
+    this.init();
+  }
+
+  draw(){
+    let centerX = this.cvs.width / 2;
+    let centerY = this.cvs.height / 2;
+
+    this.fillRect(0, 0, this.cvs.width, this.cvs.height, "black");
+    this.ctx.save();
+    this.ctx.translate(-this.mousePosition.x / 5, -this.mousePosition.y / 5)
+
+    this.stars.forEach((star)=>{
+      this.fillCircle(star.x, star.y, star.r, "white");
+    });
+
+    this.ctx.restore();
   }
 }
 
